ef2c20ec147e2067610fcc1d867c10c9b553c46c
Noam Bahar <noambahar2002@gmail.com>
2023-02-28 22:57:16 +0200

    feat(global): Add custom colors and font family to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ['./src/**/*.{html,js}', './index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        grayishblue: 'hsl(220, 15%, 55%)',
+        darkblue: 'hsl(218, 44%, 22%)',
+        lightgray: 'hsl(212, 45%, 89%)',
+      },
+      fontFamily: {
+        outfit: 'Outfit, sans',
+      },
+    },
   },
   plugins: [],
 };
